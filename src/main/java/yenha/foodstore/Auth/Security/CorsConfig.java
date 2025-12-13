f9176c9aeea63b5362732983980600c1aaa33ba6
Nguyễn Thế Anh <89338550+Theanh2911@users.noreply.github.com>
2025-12-13 20:32:52 +0700

    update cors

--- src/main/java/yenha/foodstore/Auth/Security/CorsConfig.java
+++ src/main/java/yenha/foodstore/Auth/Security/CorsConfig.java
@@ -3,15 +3,16 @@ package yenha.foodstore.Auth.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
 @Configuration
-public class CorsConfigurationSource {
+public class CorsConfig {
     
     @Bean
-    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
@@ -29,3 +30,4 @@ public class CorsConfigurationSource {
         return source;
     }
 }
+
